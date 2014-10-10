789cb8272110c06eb72084408252553b409e3837
Ordon <ordon1313@gmail.com>
2014-10-11 00:31:53 +0200

    code review

--- app/src/main/java/com/ordonteam/hackzurich/mode/JoinGameLayout.groovy
+++ app/src/main/java/com/ordonteam/hackzurich/mode/JoinGameLayout.groovy
@@ -1,7 +1,5 @@
 package com.ordonteam.hackzurich.mode
-
 import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.widget.Button
 import android.widget.EditText
@@ -15,17 +13,21 @@ class JoinGameLayout extends CenteredLayout{
     EditText hostIpAddress
     JoinGameLayout(Activity activity) {
         super(activity)
+
         TextView hostIpInfo = new TextView(activity)
-        hostIpInfo.setText("Please specify host IP:")
+        hostIpInfo.setText('Please specify host IP:')
+        addView(hostIpInfo)
+
         hostIpAddress = new EditText(activity)
+        hostIpAddress.setText('192.168.1.')
+        addView(hostIpAddress)
+
         Button joinGame = new Button(activity)
-        joinGame.setText("Join game")
+        joinGame.setText('Join game')
         joinGame.setOnClickListener({
             Intent intent = new Intent(activity, MainActivity.class)
             activity.startActivity(intent)
         })
-        addView(hostIpInfo)
-        addView(hostIpAddress)
         addView(joinGame)
     }
 }
