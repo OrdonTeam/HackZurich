f1a67cd0c7e0f60326d5f4c7667a54774095afc7
Ordon <ordon1313@gmail.com>
2014-10-10 21:44:38 +0200

    Mode Ac

--- app/src/main/java/com/ordonteam/hackzurich/MainActivity.groovy
+++ app/src/main/java/com/ordonteam/hackzurich/MainActivity.groovy
@@ -1,6 +1,7 @@
 package com.ordonteam.hackzurich
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import groovy.transform.CompileStatic
@@ -13,6 +14,10 @@ class MainActivity extends Activity {
         super.onCreate(savedInstanceState)
         def layout = new Button(this)
         layout.setText('Hello Zurich')
+        layout.setOnClickListener({
+            Intent intent = new Intent(this, ModeSelectorActivity.class)
+            startActivity(intent)
+        })
         setContentView(layout);
     }
 }
---
+++ app/src/main/java/com/ordonteam/hackzurich/ModeSelectorActivity.groovy
@@ -0,0 +1,8 @@
+package com.ordonteam.hackzurich
+
+import android.app.Activity
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class ModeSelectorActivity extends Activity {
+}
