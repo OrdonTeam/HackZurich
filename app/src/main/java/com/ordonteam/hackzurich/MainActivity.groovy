2bf9343567fcff3df2162ce1c19f4a5cc71cddef
Dariusz Szablowski <daruziek@gmail.com>
2014-10-10 22:32:56 +0200

    CenteredLayout

--- app/src/main/java/com/ordonteam/hackzurich/MainActivity.groovy
+++ app/src/main/java/com/ordonteam/hackzurich/MainActivity.groovy
@@ -4,6 +4,9 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
 import groovy.transform.CompileStatic
 
 @CompileStatic
@@ -12,12 +15,27 @@ class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState)
-        def layout = new Button(this)
-        layout.setText('Hello Zurich')
-        layout.setOnClickListener({
+
+        CenteredLayout centeredLayout = new CenteredLayout(this)
+        centeredLayout.setOrientation(LinearLayout.VERTICAL);
+        centeredLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT))
+
+        TextView titleView = new TextView(this)
+        titleView.setText("Your name:")
+        centeredLayout.addView(titleView)
+
+        EditText editText = new EditText(this)
+        centeredLayout.addView(editText)
+
+        Button button = new Button(this)
+        button.setText('Next')
+        button.setOnClickListener({
             Intent intent = new Intent(this, ModeSelectorActivity.class)
             startActivity(intent)
         })
-        setContentView(layout);
+
+        centeredLayout.addView(button)
+
+        setContentView(centeredLayout)
     }
 }
