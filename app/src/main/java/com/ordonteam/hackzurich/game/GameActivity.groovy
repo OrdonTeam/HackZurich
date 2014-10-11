aff42a56477a7acefc0d9b1ceec4196179cd5271
wokator <michal.wlaz@gmail.com>
2014-10-11 17:51:42 +0200

    cant really remember what ive done...

--- app/src/main/java/com/ordonteam/hackzurich/game/GameActivity.groovy
+++ app/src/main/java/com/ordonteam/hackzurich/game/GameActivity.groovy
@@ -1,7 +1,14 @@
 package com.ordonteam.hackzurich.game
 
 import android.app.Activity
+import android.graphics.Canvas
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.WindowManager
+import android.widget.RelativeLayout
+import android.widget.TextView
 import com.ordonteam.hackzurich.gameserver.ClientCallback
 import com.ordonteam.hackzurich.gameserver.GameServerSocket
 import com.ordonteam.hackzurich.sensors.AccelerometerActivator
@@ -13,12 +20,36 @@ class GameActivity extends Activity implements ClientCallback{
 
     GameServerSocket gameServerSocket
     GameLayout gameLayout
+    RelativeLayout gameLayoutWrapper
+    RelativeLayout curtain
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState)
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        gameLayoutWrapper = new RelativeLayout(this)
+        RelativeLayout.LayoutParams lp1 = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,RelativeLayout.LayoutParams.MATCH_PARENT)
+        gameLayoutWrapper.setLayoutParams(lp1)
+
         gameLayout = new GameLayout(this)
-        setContentView(gameLayout)
+        gameLayout.setLayoutParams(lp1)
+        gameLayoutWrapper.addView(gameLayout)
+
+        curtain = new RelativeLayout(this)
+        curtain.setLayoutParams(lp1)
+        curtain.setBackgroundColor(Color.argb(190,0,0,0))
+        gameLayoutWrapper.addView(curtain)
+
+        TextView waitInfo = new TextView(this)
+        waitInfo.setText('Please wait for your opponent')
+        waitInfo.setTextColor(Color.WHITE)
+        RelativeLayout.LayoutParams lp2 = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,RelativeLayout.LayoutParams.WRAP_CONTENT)
+        lp2.addRule(RelativeLayout.CENTER_IN_PARENT)
+        waitInfo.setLayoutParams(lp2)
+        curtain.addView(waitInfo)
+
+        setContentView(gameLayoutWrapper)
         GameServerSocket.getGameServerSocket().setClientCallback(this)
         new VoiceActivator({
             gameLayout.charge()
@@ -37,6 +68,9 @@ class GameActivity extends Activity implements ClientCallback{
     @Override
     void onStarted() {
         gameServerSocket.ready()
+        gameLayoutWrapper.post({
+            gameLayoutWrapper.removeView(curtain)
+        })
     }
 
     @Override
