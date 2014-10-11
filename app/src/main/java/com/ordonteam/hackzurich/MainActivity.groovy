df497fa8cf70c1a2537bc37de1d0dad55868972e
bad-samaritan <mdrzymala@loken.pl>
2014-10-11 04:00:31 +0200

    push push

--- app/src/main/java/com/ordonteam/hackzurich/MainActivity.groovy
+++ app/src/main/java/com/ordonteam/hackzurich/MainActivity.groovy
@@ -11,6 +11,8 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
+import com.ordonteam.hackzurich.mode.CreateGameLayout
+import com.ordonteam.hackzurich.mode.JoinGameLayout
 import com.ordonteam.hackzurich.mode.ModeSelectorActivity
 import com.ordonteam.hackzurich.util.ButtonTextLayout
 import groovy.transform.CompileStatic
@@ -21,7 +23,20 @@ class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState)
-        ButtonTextLayout layout = new ButtonTextLayout(this)
-        setContentView(layout)
+
+        LinearLayout linearLayout = new LinearLayout(this);
+        linearLayout.setOrientation(LinearLayout.VERTICAL)
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT, 1)
+
+        ButtonTextLayout content = new ButtonTextLayout(this)
+        content.setLayoutParams(layoutParams)
+        linearLayout.addView(content)
+
+        CenteredLayout layout2 = new CenteredLayout(this)
+        layout2.setLayoutParams(layoutParams)
+        linearLayout.addView(layout2)
+
+        setContentView(linearLayout)
     }
 }
