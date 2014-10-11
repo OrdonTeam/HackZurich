5db85ecd897b310799e5e9922b0e36420288c7fb
bad-samaritan <mdrzymala@loken.pl>
2014-10-11 03:06:01 +0200

    nothing hill

--- app/src/main/java/com/ordonteam/hackzurich/MainActivity.groovy
+++ app/src/main/java/com/ordonteam/hackzurich/MainActivity.groovy
@@ -1,8 +1,12 @@
 package com.ordonteam.hackzurich
 import android.app.Activity
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.Gravity
+import android.view.KeyEvent
 import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
@@ -12,6 +16,11 @@ import groovy.transform.CompileStatic
 
 @CompileStatic
 class MainActivity extends Activity {
+//implements DialogInterface.OnKeyListener
+
+    TextView titleView;
+    EditText editText;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,23 +30,39 @@ class MainActivity extends Activity {
         centeredLayout.setOrientation(LinearLayout.VERTICAL);
         centeredLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT))
 
-        TextView titleView = new TextView(this)
+        titleView = new TextView(this)
         titleView.setText("Your name:")
         titleView.setGravity(Gravity.CENTER_HORIZONTAL)
         centeredLayout.addView(titleView)
 
-        EditText editText = new EditText(this)
+        button = new Button(this)
+        button.setText('Next')
+        button.setOnClickListener({
+            Intent intent = new Intent(this, ModeSelectorActivity.class)
+            intent.putExtra("nick", editText.text);
+            startActivity(intent)
+        })
+        button.setEnabled(false);
+
+        editText = new EditText(this)
         float scale = getResources().getDisplayMetrics().density;
         int dpAsPixels = (int) (200*scale + 0.5f);
         editText.setLayoutParams(new LinearLayout.LayoutParams(dpAsPixels, LinearLayout.LayoutParams.WRAP_CONTENT))
         centeredLayout.addView(editText)
+        editText.addTextChangedListener(new TextWatcher(){
+            public void afterTextChanged(Editable s) {
+                if (editText.text.toString()?.length() == 0){
+                    button.setEnabled(false);
+                }
+                else{
+                    button.setEnabled(true);
+                }
+            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            public void onTextChanged(CharSequence s, int start, int before, int count){}
+        });
 
-        Button button = new Button(this)
-        button.setText('Next')
-        button.setOnClickListener({
-            Intent intent = new Intent(this, ModeSelectorActivity.class)
-            startActivity(intent)
-        })
+
 
         centeredLayout.addView(button)
 
